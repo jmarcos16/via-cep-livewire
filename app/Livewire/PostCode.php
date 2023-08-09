98a0b86920bb404a0fc1b709671d2055cae20ec2
Zé Marcos <contato.marcosrf@gmail.com>
2023-08-08 21:59:21 -0300

    finish search in api

--- app/Livewire/PostCode.php
+++ app/Livewire/PostCode.php
@@ -2,10 +2,41 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class PostCode extends Component
 {
+    public int $postCode;
+
+    public string $host = 'https://viacep.com.br/ws/{cep}/json/';
+
+
+    public string $address = '';
+
+    public string $neighborhood = '';
+
+    public string $city = '';
+
+
+    public function searchPostCode()
+    {
+
+        $this->validate([
+            'postCode' => 'required|numeric|digits:8'
+        ]);
+
+        $url = str_replace('{cep}', $this->postCode, $this->host);
+
+        $response = Http::get($url)->object();
+
+        $this->address = $response->logradouro;
+        $this->neighborhood = $response->bairro;
+        $this->city = $response->localidade;
+
+    }
+
+
     public function render()
     {
         return view('livewire.post-code');
--- resources/views/livewire/post-code.blade.php
+++ resources/views/livewire/post-code.blade.php
@@ -2,28 +2,30 @@
 
     <div class="flex w-full my-3">
         <x-text-input class="rounded-r-none w-10/12 px-4 py-3" label="CEP" placeholder="Provide PostCode"
-            wire:model="postcode" />
+            wire:model="postCode" />
 
-        <x-primary-button wire:click="searchCep" class="rounded-l-none">
+        <x-primary-button wire:click="searchPostCode" class="rounded-l-none">
             Search
         </x-primary-button>
     </div>
 
 
     <div>
-        <x-input-label for="address" class="mt-3">Address</x-input-label>
-        <x-text-input id="address" class="w-full" wire:model="address" disabled placeholder="Your Address" />
+        <x-input-label for="address" class="mt-2">Address</x-input-label>
+        <x-text-input id="address" class="w-full" wire:model="address" disabled placeholder="Your Address"
+            value="{{ $address }}" />
     </div>
 
     <div>
-        <x-input-label for="neighborhood" class="mt-3">Neighborhood</x-input-label>
+        <x-input-label for="neighborhood" class="mt-2">Neighborhood</x-input-label>
         <x-text-input id="neighborhood" class="w-full" wire:model="neighborhood" disabled
-            placeholder="Your Neighborhood" />
+            placeholder="Your Neighborhood" value="{{ $neighborhood }}" />
     </div>
 
     <div>
-        <x-input-label for="city" class="mt-3">City</x-input-label>
-        <x-text-input id="city" class="w-full" wire:model="city" disabled placeholder="Your City" />
+        <x-input-label for="city" class="mt-2">City</x-input-label>
+        <x-text-input id="city" class="w-full" wire:model="city" disabled placeholder="Your City"
+            value="{{ $city }}" />
     </div>
 
 
